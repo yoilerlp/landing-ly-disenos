ff9bddbfb72643533300bca6eff7f6ce686d7ab6
Yoyler Cordoba <yoilerlp595@gmail.com>
2025-12-10 19:41:41 -0500

    Add mobile logo asset in WEBP format, adjust privacy

--- src/components/common/header/HeaderWrapper.tsx
+++ src/components/common/header/HeaderWrapper.tsx
@@ -20,7 +20,7 @@ const HeaderWrapper = ({ children, className }: Props) => {
         className={cn(
           'w-full flex justify-between items-center px-2 xl:px-14 py-6 transition-all duration-300 will-change-transform max-sm:scale-[0.9]',
           {
-            'rounded-[40px] py-4 w-[95%] bg-white/10 backdrop-blur-[32px] translate-y-2 md:translate-y-5':
+            'rounded-[40px] py-4 w-[98%] sm:w-[95%] bg-white/10 backdrop-blur-[32px] translate-y-2 md:translate-y-5':
               scrolled,
             'border-b border-white/20 shadow-lg': scrolled,
           }
